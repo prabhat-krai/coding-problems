5b3c552fbad9d18ec2674f890877309fd232e9fe
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2018-11-19 00:47:53 +0530

    completed

---
+++ modarray.py
@@ -0,0 +1,20 @@
+"""
+This program makes a new array where every element is replace my the total
+product of all elements divided by the element at that position in original array.
+division is allowed.
+
+"""
+def modarray(num, product):
+    new_array=[product//num[i] for i in range(len(num))]
+
+    return new_array
+
+num=[1,2,3,4,5,6,7,8,9,10]
+product=1
+for j in range(len(num)):
+    product=product*num[j]
+
+print(modarray(num, product))
+
+
+
