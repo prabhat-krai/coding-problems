1e7193243cac4494f88e8ef826d6c6ed485581d1
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2018-11-19 00:10:03 +0530

    completed

--- firstproblem.py
+++ firstproblem.py
@@ -3,7 +3,22 @@ This program finds if a pair of numbers that equal to a user inputted number
 
 """
 a=[1,2,7,2,6,2,6,8,5,3,2,6,8,9,89]
-k=6
-a=sort(a)
+k=14
+a=sorted(a)
 l=0
 r=len(a)-1
+
+while(l<r):
+    if(a[l]+a[r]==k):
+        print("True")
+        break
+    else: 
+        if(a[l]+a[r]<k):
+            l+=1
+        else: 
+            r-=1
+
+if(l==r):
+    print("False")
+
+
