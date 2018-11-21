ebd00659b05984168c09f268a53c66b904aaf52d
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2018-11-21 16:46:38 +0530

    completed

--- minpositive.py
+++ minpositive.py
@@ -4,5 +4,53 @@ and constant space. In other words, find the lowest positive integer that does n
 in the array. The array can contain duplicates and negative numbers as well.
 
 """
+import math
 
-# currently thinking about this. 
+def seperate_posandneg(num, size):
+    j=0
+    for i in range(size):
+        if (num[i] <= 0):
+            temp = num[i]
+            num[i]= num[j]
+            num[j]=temp
+
+            j+=1
+
+    return num, j
+
+
+def findMissing(arr, size):
+    
+    for i in range(size):
+        
+        x=int(math.fabs(arr[i]))
+        if( x - 1 < size and arr[x-1] > 0):
+            arr[x-1]= -1 * arr[x-1] 
+    i=0
+    for i in range(size):
+        if (arr[i] > 0):
+            return i+1
+
+    return size+1
+
+
+def form_arrray(arr, size):
+
+    arr_sep, negs= seperate_posandneg(arr, len(arr))
+    j=0
+    arr2=[None]*(size-negs)
+    for i in range (negs, size):
+        arr2[j]=arr_sep[i]
+        j+=1
+
+    return findMissing(arr2, len(arr2))
+
+
+
+a=[1,3, -4, 2]
+
+print(form_arrray(a, len(a)))
+
+
+
+
