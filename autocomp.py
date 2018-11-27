dec94507d167750fc2ba6bda3765dc4d434a56c5
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2018-11-27 11:49:57 +0530

    big backlog

---
+++ autocomp.py
@@ -0,0 +1,8 @@
+"""
+Implement an autocomplete system. That is, given a query string s
+and a set of all possible query strings, return all strings in the
+set that have s as a prefix.
+
+"""
+
+#thinking of a fast way to search
