74632a7b95f1fdbfe77b37f3449bd94fc43cf508
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-02-09 13:10:41 +0530

    Completed

--- montcarloradius.py
+++ montcarloradius.py
@@ -7,3 +7,30 @@ Hint: The basic equation of a circle is x2 + y2 = r2.
 
 """
 #learning to run a monte carlo simulation
+
+import random
+import math
+import numpy as np
+# Number of points inside the circle
+inside = 0
+# Total number of points to be generated
+total = 1000
+pi_list =[]
+
+for _ in range(100):
+    for i in range(0, total):
+        # Generate random x, y in [0, 1].
+        x2 = random.random()**2
+        y2 = random.random()**2
+        # Increment if inside unit circle.
+        if(math.sqrt(x2 + y2) < 1.0):
+            inside += 1
+
+    # inside / total = pi / 4
+    pi = (float(inside) / total) * 4
+    pi_list.append(pi)
+    pi=0.0
+    inside=0
+
+
+print(np.mean(pi_list))
