c4d30907ac33dd6a8ee5f4e439b989619cc1f3ae
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-06-29 17:55:32 +0530

    Working now. Using DP.

--- min_coins.py
+++ min_coins.py
@@ -1,2 +1,12 @@
 def find_min_coins(n, denominations):
-    
+    coins = [float("inf") for a in range (n+1)]
+    coins[0] = 0
+    for denomination in denominations:
+        for amount in range(n + 1):
+            if(denomination <= amount):
+                coins[amount] = min(coins[amount] , coins[amount - denomination] + 1)
+
+    result = -1 if coins[n] == float("inf") else coins[n]
+    return result
+
+print(find_min_coins(9,[1,5,10]))
