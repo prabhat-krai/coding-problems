d1685769c859aaff1f2e626ed92d2279e9e931ff
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-07-04 17:59:29 +0530

    Problem fixed. Had an index issue.

--- levenshtein.py
+++ levenshtein.py
@@ -3,14 +3,14 @@ def levenshteinDistance(str_1, str_2):
     for i in range(1, len(str_2)+1):
         changes[i][0] = changes[i-1][0] + 1
 
-    for r in range(1, len(str_1) + 1):
-        for c in range(1, len(str_2) + 1):
+    for r in range(1, len(str_2) + 1):
+        for c in range(1, len(str_1) + 1):
 
-            if(str_1[r - 1] == str_2[c - 1]):
+            if(str_1[c - 1] == str_2[r - 1]):
                 changes[r][c] = changes[r - 1][c - 1]
             else:
                 changes[r][c] = min(changes[r - 1][c - 1], changes[r - 1][c], changes[r][c - 1]) + 1   
 
     return changes[-1][-1]
 
-print(levenshteinDistance("11/342, Vikas", "11/343, Vikas Nagar"))
+print(levenshteinDistance("Bangalore", "Bengaluru"))
