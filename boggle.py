5a758c6fbd2b4a59690dcef84f6cc503be32a2dd
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-07-20 19:26:26 +0530

    Wrong approach for now

--- boggle.py
+++ boggle.py
@@ -2,7 +2,19 @@ def boggleBoard(board, words):
     check_words = {}
     for word in words:
         check_words[word] = True
-    if word in check_words:
-        print("Works")
     
-boggleBoard([], ["Pranav", "Prabhat"])
+    x = 0
+    y = 0
+    currWord = board[x][y]
+    findWords(currWord, board, check_words, x+1, y)
+    findWords(currWord, board, check_words, x-1, y)
+    findWords(currWord, board, check_words, x, y+1)
+    findWords(currWord, board, check_words, x+1, y-1)
+    findWords(currWord, board, check_words, x+1, y+1)
+    findWords(currWord, board, check_words, x+1, y-1)
+    findWords(currWord, board, check_words, x-1, y+1)
+    findWords(currWord, board, check_words, x-1, y-1)
+
+
+def findWords(word, board, words, x, y):
+    
