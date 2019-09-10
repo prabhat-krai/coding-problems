abc7e33c64730981b232e13b42d8f5ec65f72419
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-10 16:37:37 +0530

    Fixed indentation

--- decodelex.py
+++ decodelex.py
@@ -8,23 +8,23 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 
 def decode(lex, n):  
   
-    count = [0] * (n + 1); 
-    count[0] = 1;  
-    count[1] = 1;  
+    count = [0] * (n + 1)
+    count[0] = 1
+    count[1] = 1
   
     for i in range(2, n + 1):  
   
-        count[i] = 0;  
+        count[i] = 0
  
         if (lex[i - 1] > '0'):  
-            count[i] = count[i - 1];  
+            count[i] = count[i - 1]
    
         if (lex[i - 2] == '1' or 
            (lex[i - 2] == '2' and 
             lex[i - 1] < '7') ):  
-            count[i] += count[i - 2];  
+            count[i] += count[i - 2]
   
-    return count[n];  
+    return count[n]
   
 lex = "1111129"
 n = len(lex)
