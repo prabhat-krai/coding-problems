81cd59abe39c4f1c2261abc94e623a1c47103476
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-10 16:36:38 +0530

    Working DP solution

--- decodelex.py
+++ decodelex.py
@@ -6,23 +6,26 @@ For example, the message '111' would give 3, since it could be decoded as 'aaa',
 You can assume that the messages are decodable. For example, '001' is not allowed.
 """
 
-def decode(lex, dic):
-    for i in range(1,3):
-        char = lex[i -1]
-        print(dic[char])
-        decode(lex[i:], dic)
-
-
-
-
-
-
-
-
-
-
-
-
-dic = {str(i - 96): str(chr(i)) for i in range(97, 123)}
-
-decode('1123', dic)
+def decode(lex, n):  
+  
+    count = [0] * (n + 1); 
+    count[0] = 1;  
+    count[1] = 1;  
+  
+    for i in range(2, n + 1):  
+  
+        count[i] = 0;  
+ 
+        if (lex[i - 1] > '0'):  
+            count[i] = count[i - 1];  
+   
+        if (lex[i - 2] == '1' or 
+           (lex[i - 2] == '2' and 
+            lex[i - 1] < '7') ):  
+            count[i] += count[i - 2];  
+  
+    return count[n];  
+  
+lex = "1111129"
+n = len(lex)
+print(decode(lex, n))
