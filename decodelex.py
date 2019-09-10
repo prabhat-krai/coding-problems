d4c7cdd48a88cbc872d52c5b8f723170d406f38f
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-10 15:36:20 +0530

    Working on recursion

--- decodelex.py
+++ decodelex.py
@@ -6,4 +6,23 @@ For example, the message '111' would give 3, since it could be decoded as 'aaa',
 You can assume that the messages are decodable. For example, '001' is not allowed.
 """
 
-#currently thinking
+def decode(lex, dic):
+    for i in range(1,3):
+        char = lex[i -1]
+        print(dic[char])
+        decode(lex[i:], dic)
+
+
+
+
+
+
+
+
+
+
+
+
+dic = {str(i - 96): str(chr(i)) for i in range(97, 123)}
+
+decode('1123', dic)
