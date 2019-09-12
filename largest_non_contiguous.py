18dadb41f67c4b181554145dcf914f01033e0401
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-12 12:47:09 +0530

    added comment

--- largest_non_contiguous.py
+++ largest_non_contiguous.py
@@ -1,3 +1,6 @@
+#solving the problem of max sum where the elements forming the sum
+#are not contiguous
+
 def sum_not_contiguous(arr):
     current_max = 0
     max_so_far = 0
@@ -9,5 +12,5 @@ def sum_not_contiguous(arr):
 
     return (max_so_far if max_so_far > current_max else current_max)
 
-arr = [5,5,10,100,10,5]
+arr = [5,5,10,100,1000,50]
 print(sum_not_contiguous(arr))
