86131e274b35cc1cf21b0cb2d9ed18ea7994036f
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-30 19:20:21 +0530

    Added methods to Trie class

--- longest_compound_word.py
+++ longest_compound_word.py
@@ -1,11 +1,21 @@
 """Given a sorted list of words, find the longest compound word in the 
 list that is constructed by concatenating the words in the list"""
 
+import collections
+
 class Node:
     def __init__(self, letter = None, isTerminal = False):
         self.letter = letter
         self.children = {}
         self.isTerminal = isTerminal
+
+class Trie:
+    def __init__(self):
+        self.root = Node('')
+    
+    def __repr__(self):
+        self.output([self.root])
+        return ''
 
     def insert(self, word):
         current = self.root
@@ -28,5 +38,41 @@ class Node:
                 prefixes.append(prefix)
         return prefixes
 
-        
-        
+
+    def __contains__(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current.children:
+                return False
+            else:
+                current = current.children[letter]
+        return current.isTerminal
+
+    def output(self, currentPath, indent=''):
+        currentNode=currentPath[-1]
+        if currentNode.isTerminal:
+            word=''.join([node.letter for node in currentPath])
+            print indent+word
+            indent+='  '
+        for letter, node in sorted(currentNode.children.items()):
+            self.output(currentPath[:]+[node], indent)
+
+
+
+
+
+def longestWord(words):
+    trie = Trie()
+    queue = collections.deque()
+    for word in words:
+        prefixes = trie.getAllPrefixesofWord(word)
+        for prefix in prefixes:
+            queue.append((word, word[len(prefix):]))
+        trie.insert(word)
+
+    longestWord =''
+    maxLength = 0
+    while queue:
+        word, suffix = queue.popleft()
+        if suffix in trie and len(word) > maxLength:
+
