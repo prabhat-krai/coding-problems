6cf7fbc66e6c9b7a7b5e6ceee17105a34ca8ac88
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-30 13:52:55 +0530

    test cases added

---
+++ reverse_vowels.py
@@ -0,0 +1,22 @@
+vowels = set(('a', 'e', 'i', 'o', 'u'))
+
+def reverse_vowels(sentence):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+def test():
+    assert reverse_vowels("hello") == "holle"
+    assert reverse_vowels("papergrid") == "pipergred"
+    assert reverse_vowels("TTT") === "ttt"
+    assert reverse_vowels("AEIO") === "oiea"
+    print("Test cases passed")
