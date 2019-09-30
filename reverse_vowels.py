d0c3a0bd26790287c43c1ef263912e5131df6c81
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-09-30 14:07:44 +0530

    Working on test cases

--- reverse_vowels.py
+++ reverse_vowels.py
@@ -1,10 +1,32 @@
 vowels = set(('a', 'e', 'i', 'o', 'u'))
 
 def reverse_vowels(sentence):
-    pass
+    sentence = sentence.lower()
+    if (len(sentence) == 0):
+        return ""
+    i = 0 
+    j = len(sentence) - 1
+    while(i<j):
+        if(sentence[i]  in vowels and sentence[j]  in vowels):
+            sentence = swap(sentence, i, j)
+            i+=1
+            j-=1
+        elif (sentence[i]  in vowels and not (sentence[j]  in vowels)):
+            j -=1
+        elif (not (sentence[i]  in vowels) and sentence[j]  in vowels):
+            i +=1
+        else:
+            i+=1
+            j-=1
+    print(sentence)
+    return sentence
 
 
 
+def swap(c, i, j):
+    c = list(c)
+    c[i], c[j] = c[j], c[i]
+    return ''.join(c)
 
 
 
@@ -16,7 +38,9 @@ def reverse_vowels(sentence):
 
 def test():
     assert reverse_vowels("hello") == "holle"
-    assert reverse_vowels("papergrid") == "pipergred"
-    assert reverse_vowels("TTT") === "ttt"
-    assert reverse_vowels("AEIO") === "oiea"
+    assert reverse_vowels("papergrid") == "pipergrad"
+    assert reverse_vowels("TTT") == "ttt"
+    assert reverse_vowels("AEIO") == "oiea"
     print("Test cases passed")
+
+test()
