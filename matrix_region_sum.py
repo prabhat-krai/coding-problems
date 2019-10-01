74df2f561c8560c3a448c66925ec624c80a5dc4c
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-02 02:08:34 +0530

    Added precomputation step

--- matrix_region_sum.py
+++ matrix_region_sum.py
@@ -18,3 +18,24 @@ def matrix_region_sum_slower(matrix, A, D):
 #This solution will calculate the regions sum everytime.
 #This will slow down the process.
 
+def precompute_sums(matrix):
+    """Using Dynamic programming to build the array of sums.
+       This reduces the time needed to build the sums matrix."""
+    topRow, bottomRow=(0, len(matrix)-1)
+    leftCol, rightCol=(0, len(matrix[0])-1)
+    sums=[[0]*(rightCol-leftCol+1) for i in range(bottomRow-topRow+1)]
+    sums[topRow][leftCol]=matrix[topRow][leftCol]
+ 
+    for col in range(leftCol+1, rightCol+1):
+        sums[topRow][col]=sums[topRow][col-1]+matrix[topRow][col]
+    for row in range(topRow+1, bottomRow+1):
+        sums[row][leftCol]=sums[row-1][leftCol]+matrix[row][leftCol]
+ 
+    for col in range(leftCol+1, rightCol+1):
+        columnSum=matrix[topRow][col]
+        for row in range(topRow+1, bottomRow+1):
+            sums[row][col]=sums[row][col-1]+matrix[row][col]+columnSum
+            columnSum+=matrix[row][col]
+ 
+    return sums
+
