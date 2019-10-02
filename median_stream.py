6774fe2b34177385f523be9eca019533bec6abbe
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-02 22:29:32 +0530

    Added the logic

--- median_stream.py
+++ median_stream.py
@@ -2,3 +2,9 @@
 
 #I will use two heaps to have access of the median in O(1) time. 
 
+"""
+This forms a min heap and other is max heap. I take the first element in min heap and then compare
+the next element to this element into min or max heap. If the heaps are imbalanced by more than one 
+element. We rebalance the heaps by shifting elements.
+"""
+
