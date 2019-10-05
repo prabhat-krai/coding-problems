79635da4525ce4d5d434a3120e04b8e09ff8d2e4
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-05 21:08:55 +0530

    Thinking

--- digit_dp.py
+++ digit_dp.py
@@ -3,15 +3,15 @@ Given a left and right ends of a list.
 Find all the numbers between the two that sum to N.
 """
 
-left = int(input("Left end"))
-right = int(input("Right end"))
-sum_total = int(input("Sum"))
+left = int(input("Left end : "))
+right = int(input("Right end : "))
+sum_total = int(input("Sum : "))
 
 #naive solution
 
 def get_sum(num, sum_total):
     num_sum = 0
-    while(num > 0 and num_sum <= sum_total):
+    while(num > 0): #keeping a check for num_sum <= sum_total is slowing the program
         d = num % 10
         num_sum += d
         num = num // 10
@@ -22,10 +22,11 @@ for i in range(left, right+1):
     num_sum = get_sum(i, sum_total)
     if(num_sum == sum_total):
         count += 1
-
 print(count)
 
 
 #Dynammic Programming
+
+
 
 
