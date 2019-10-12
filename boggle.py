4feb3a7934773d4587592fdd6677c0b56fa2a67d
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-12 12:33:02 +0530

    Working

--- boggle.py
+++ boggle.py
@@ -15,33 +15,63 @@ def boggleBoard(board, words):
             explore(i, j, board, trie.root, visited, finalWords)
     return list(finalWords.keys())
 
+def explore(i, j, board, trieNode, visited, finalWords):
+    if visited[i][j]:
+        return
+    letter = board[i][j]
+    if letter not in trieNode:
+        return
+    visited[i][j] = True
+    trieNode = trieNode[letter]
+    if "*" in trieNode:
+        finalWords[trieNode["*"]] = True
+    neighbors = getNeighbors(i, j, board)
+    for neighbor in neighbors:
+        explore(neighbor[0], neighbor[1], board, trieNode, visited, finalWords)
+    visited[i][j] = False
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def getNeighbors(i, j, board):
+    neighbors = []
+    if( i > 0 and j > 0):
+        neighbors.append([i - 1, j - 1])
+    if( i > 0 and j < len(board[0]) - 1):
+        neighbors.append([i - 1, j + 1])
+    if( i < len(board) - 1 and j < len(board[0]) - 1):
+        neighbors.append([i + 1, j + 1])
+    if( i < len(board) - 1 and j > 0):
+        neighbors.append([i + 1, j - 1])
+    if( i > 0):
+        neighbors.append([i - 1, j])
+    if( i < len(board) - 1):
+        neighbors.append([i + 1, j])
+    if(j > 0):
+        neighbors.append([i, j - 1])
+    if( j < len(board[0]) - 1):
+        neighbors.append([i, j + 1])
+    return neighbors
 
 class Trie:
     def __init__(self):
         self.root = {}
         self.end_symbol =  "*"
     
-    def add_word(self, word):
+    def add(self, word):
         current  = self.root 
         for letter in word:
             if letter not in current:
                 current[letter] = {}
             current = current[letter]
         current[self.end_symbol] = word
+
+def test():
+    board  = [['W', 'N', 'S'],
+            ['I', 'O', 'D'],
+             ['W', 'S', 'R']]
+
+    words = ['WORDS', 'WORD', "WINS", "Not present" ]
+    
+    print(boggleBoard(board, words) )
+    
+
+test()
