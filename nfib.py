3a9bc565782451a081ca00e4e0258543d7a171c9
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-16 01:00:21 +0530

    complete programs and timing them

--- nfib.py
+++ nfib.py
@@ -1,6 +1,10 @@
+import timeit
+memo = [None] * 1000
+
+
 def getNthFibIter(n):
     # Write your code here.
-	a = [1 , 1]
+	a = [0 , 1]
 	if (n==1):
 		return a[0]
 	if (n==2):
@@ -11,11 +15,35 @@ def getNthFibIter(n):
 	return a[n-1]
 
 def getNthFibRec(n):
-	if (n == 1 or n==2):
-		return 1
+	if (n == 1):
+		return 0
+	if (n == 2):
+		return 1	
 	else:
 		return getNthFibRec(n - 1) + getNthFibRec(n - 2)
 
 
-print(getNthFibIter(20))
-print(getNthFibRec(20))
+def getNthFibRecDP(n, memo):
+	if (n == 2 or n==3):
+		memo[n] = 1
+	if(memo[n] is None):
+		memo[n] = getNthFibRecDP(n - 1, memo) + getNthFibRecDP(n - 2, memo)
+	
+	return memo[n]
+
+
+
+def wrapper(func, *args, **kwargs): # wrapper for function to time them
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+exp = 1  # number of times to run them
+fib = 40 # which fib number
+
+wrapped = wrapper(getNthFibIter, fib)
+print("Iteration Time : ",timeit.timeit(wrapped, number = exp))
+wrapped = wrapper(getNthFibRecDP, fib, memo)
+print("Recursion Time with DP : ",timeit.timeit(wrapped, number = exp))
+wrapped = wrapper(getNthFibRec, fib)
+print("Recursion Time : ",timeit.timeit(wrapped, number = exp))
