6225a76c2829b0c4dd38387d1376376b16629b77
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-31 23:26:55 +0530

    Tests added

--- next_larger.py
+++ next_larger.py
@@ -3,14 +3,22 @@
 
 def next_larger_entry(arr): 
     result = []
-    for i in range(0, len(arr), 1): 
+    for i in range(0, len(arr)): 
         next = -1
-        for j in range(i+1, len(arr), 1): 
-            if arr[i] < arr[j]: 
+        for j in range(i+1, len(arr)): 
+            if (arr[i] < arr[j]): 
                 next = arr[j] 
                 break
         result.append(next)      
     return result
   
-arr = [11,13,21,3] 
-print(next_larger_entry(arr))
+
+def test():
+    arr = [11,13,21,3] 
+    assert next_larger_entry(arr) == [13,21,-1,-1]
+    arr = [1,2,3,4,5]
+    assert next_larger_entry(arr) == [2,3,4,5,-1]
+
+    print('tests passed')
+
+test()
