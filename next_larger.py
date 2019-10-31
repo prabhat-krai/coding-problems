f024056521114c4f23aca34cf0f83d0f29731d62
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-10-31 23:24:11 +0530

    Working

--- next_larger.py
+++ next_larger.py
@@ -1,2 +1,16 @@
 # The Next greater Element for an element x is the first 
 # greater element on the right side of x in array.
+
+def next_larger_entry(arr): 
+    result = []
+    for i in range(0, len(arr), 1): 
+        next = -1
+        for j in range(i+1, len(arr), 1): 
+            if arr[i] < arr[j]: 
+                next = arr[j] 
+                break
+        result.append(next)      
+    return result
+  
+arr = [11,13,21,3] 
+print(next_larger_entry(arr))
