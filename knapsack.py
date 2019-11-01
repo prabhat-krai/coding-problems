97fd08ae43280bc1cc37564b035358ca4c7354d8
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-11-01 22:39:39 +0530

    added desc

--- knapsack.py
+++ knapsack.py
@@ -1,3 +1,10 @@
+"""
+Given a bag which can carry a max weight and different items of 
+varying weights and values. 
+We need to find the items that should be picked up to maximize value 
+contained in the bag.
+"""
+
 def knapsack(items, weight_bag):
     values = [[0 for i in range(weight_bag + 1)]for j in range(len(items) + 1)]
     for i in range(1, len(items)+1):
@@ -12,5 +19,3 @@ def knapsack(items, weight_bag):
     return values[-1][-1]
 
 print(knapsack([[1,4],[2,5],[5,6],[7,2]], 15))
-    
-
