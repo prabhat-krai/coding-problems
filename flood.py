46e03730cea5c0a6b4f72ae7926e4e35b445b932
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-11-12 23:56:05 +0530

    Written main code

--- flood.py
+++ flood.py
@@ -4,5 +4,12 @@
 #of water they can hold between them. 
 
 def find_water_volume(heights):
-    for i in range(len(heights)):
+    max_vol = 0
+    for i in range(len(heights) - 1):
         #trying all permutations
+        for j in range(i+1, len(heights)):
+            vol = min(heights[i], heights[j]) * (j - i)
+            if (vol > max_vol):
+                max_vol = vol 
+
+    return max_vol
