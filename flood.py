3e02b7487f2fb7c2f23db3c48634009907a193e4
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-11-13 10:46:01 +0530

    Working and tested

--- flood.py
+++ flood.py
@@ -8,8 +8,16 @@ def find_water_volume(heights):
     for i in range(len(heights) - 1):
         #trying all permutations
         for j in range(i+1, len(heights)):
-            vol = min(heights[i], heights[j]) * (j - i)
+            height_here = min(heights[i], heights[j]) 
+            vol = height_here * (j - i)
             if (vol > max_vol):
                 max_vol = vol 
 
     return max_vol
+
+def test():
+    assert find_water_volume([1,5,1,1,1,5,80]) == 25
+    assert find_water_volume([1,5,1,1,1,5,4]) == 20
+    print("Passed")
+
+test()
