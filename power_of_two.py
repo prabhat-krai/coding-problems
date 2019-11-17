799f24bfe41d188f19c4ed29747d2bf62e872abe
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-11-17 19:13:53 +0530

    Added descripption

--- power_of_two.py
+++ power_of_two.py
@@ -2,8 +2,8 @@ def isPowerOfTwo(n):
     if (n <= 0):
         return False
 
-    n = format(n, 'b') 
-    if(sum(map(int, str(n))) == 1):
+    n = format(n, 'b')  # converting it into bits interpretation
+    if(sum(map(int, str(n))) == 1): #if there is only one 1 in that then it is power of 2
         return True
     else:
         return False
