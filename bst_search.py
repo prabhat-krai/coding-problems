3a89f32b578c596d85bb0eb7ed203d80d2eb9ffd
Prabhat Kumar Rai <prabhatkumar27@gmail.com>
2019-11-25 23:47:19 +0530

    Added comments for steps

--- bst_search.py
+++ bst_search.py
@@ -2,9 +2,9 @@ def findClosestValueInBst(tree, target):
 	return findClosestValueInBstHelper(tree, target, float("inf"))
 	
 def findClosestValueInBstHelper(tree, target, closest):
-	if (tree is None):
+	if (tree is None):  #base case
 		return closest
-	if (abs(target - closest) > abs(target - tree.value)):
+	if (abs(target - closest) > abs(target - tree.value)): #assigning new closest value
 		closest = tree.value
 	
 	if (target > tree.value):
